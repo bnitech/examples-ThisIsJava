ac8141d5002260c7aad80c08b042b9ea92193783
bnitech <bnitech88@gmail.com>
2020-08-13 00:29:29 +0900

    google code style로 코드 컨벤션 맞추어 정리

--- src/com/bnitech/Hello.java
+++ src/com/bnitech/Hello.java
@@ -1,7 +1,8 @@
 package com.bnitech;
 
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello, welcome to the java world!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello, welcome to the java world!");
+  }
 }
